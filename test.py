20b57d5ee1d7a253bd0f290d68a40454652a3a9d
miroc_whyixd_desktop <miroc@whyicxd.com>
2025-05-16 15:01:39 +0800

    simpilfy connect logic

--- test.py
+++ test.py
@@ -8,29 +8,29 @@ motor_ctrl = MoonsStepper(
 motor_ctrl.connect()
 sleep(1)
 motor_ctrl.send_command(command=StepperCommand.stop_kill)
-# motor_ctrl.send("JS0.1")
-# motor_ctrl.send("@CJ")
-motor_ctrl.send_command(address="", command=StepperCommand.jog)
+# # motor_ctrl.send("JS0.1")
+# # motor_ctrl.send("@CJ")
+# motor_ctrl.send_command(address="", command=StepperCommand.jog)
+# # sleep(1)
+# for i in range(10):
+#     # motor_ctrl.change_jog_speed("", 2 * i)
+#     # motor_ctrl.send(f"JS{2 * i}")
+#     motor_ctrl.send_command(
+#         command=StepperCommand.change_jog_speed, value=1.2 * i + 0.1
+#     )
+#     # motor_ctrl.get_position("@", lambda x: print(f"Position: {x}"))
+#     motor_ctrl.get_status(
+#         "@", StepperCommand.position, callback=lambda x: print(f"Status: {x}")
+#     )
+#     sleep(0.5)
+# motor_ctrl.send_command(command=StepperCommand.stop_kill)
+# # # motor_ctrl.stop_and_kill()
+# # motor_ctrl.send("SK")
+# motor_ctrl.send_command(StepperCommand.encoder_position, 0)
+# motor_ctrl.send_command(StepperCommand.set_position, 0)
 # sleep(1)
-for i in range(10):
-    # motor_ctrl.change_jog_speed("", 2 * i)
-    # motor_ctrl.send(f"JS{2 * i}")
-    motor_ctrl.send_command(
-        command=StepperCommand.change_jog_speed, value=1.2 * i + 0.1
-    )
-    # motor_ctrl.get_position("@", lambda x: print(f"Position: {x}"))
-    motor_ctrl.get_status(
-        "@", StepperCommand.position, callback=lambda x: print(f"Status: {x}")
-    )
-    sleep(0.5)
-motor_ctrl.send_command(command=StepperCommand.stop_kill)
-# # motor_ctrl.stop_and_kill()
-# motor_ctrl.send("SK")
-motor_ctrl.send_command(StepperCommand.encoder_position, 0)
-motor_ctrl.send_command(StepperCommand.set_position, 0)
-sleep(1)
 motor_ctrl.get_status(
-    "@", StepperCommand.position, callback=lambda x: print(f"Status: {x}")
+    "@", StepperCommand.temperature, callback=lambda x: print(f"Status: {x}")
 )
 sleep(1)
 motor_ctrl.disconnect()
